6f477ed92174f72e43f7930265419f4270899f33
isham bhandari <ishambhandari007@gmail.com>
2022-01-28 18:21:27 +0545

    card component

--- src/containers/card/Card_About.jsx
+++ src/containers/card/Card_About.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "./card.css";
 
-const Card_About = () => {
+const Card_About = (props) => {
   return (
     <div className="card_container">
       <Card style={{ backgroundColor: "#d53a9d", borderRadius: "10px" }}>
-        <Card.Body>This is some text within a card body.</Card.Body>
+        <Card.Body>{props.body}</Card.Body>
       </Card>
     </div>
   );
